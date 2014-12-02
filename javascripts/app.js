47fe174d6ddcc6a55393aae77cb2c25f954652cc
DS <grok.ksl@gmail.com>
2014-12-02 12:46:10 +0530

    the content of all the tabs added and click event triggered

--- javascripts/app.js
+++ javascripts/app.js
@@ -16,30 +16,57 @@ var main = function() {
     	//create a click handler for this element
     	$(element).on("click", function(){
     		//since we are using the jQuery version of element, we'll create a temp variableso we don't need to keep recreating it
-    		var $element = $(element), 
-    		               $content;
-
+    		var $element = $(element);
+    		
+    		//create a click handler for this element
+    		$elemnt.on("click", function(){
+    			var $content,
+    			    $input,
+    			    $button,
+    			    i;
+        
     		$(".tabs a span").removeClass("active");
     		$(element).addClass("active");
     		$("main .content").empty();
 
     		if($element.parent().is(":nth-child(1)")){
     			console.log("first tab clicked");
+    			//newest first, so we have to go through the array backwards
+    			$content = $("<ul>");
+    			for (i = toDos.length-1; i>=0; i--){
+    				$content.append($("<li>").text(toDos[i]));
+    			}
     		}else if ($element.parent().is(":nth-child(2)")){
+    			//oldest first so we go through thearray forward
     			$content = $("<ul>");
     			toDos.forEach(function (todo){
     				$content.append($("<li>").text(todo));
     			});
-    			$("main .content").append($content);
     			console.log("second tab clicked");
     		}
     		else if ($element.parent().is(":nth-child(3)")){
     			console.log("third tab clicked");
+    			//input a new to-do
+    			$input = $("<input>");
+    			$button = $("<button").text("+");
+
+    			$button.on("click", function(){
+    				if($input.val() !== ""){
+    					toDos.push($input.val());
+    					$input.val("");
+    				}
+    			});
+
+    			$content = $("<div>").append($input).append($button);
+    			/*Alternatively append() allows multiple arguments so the above 
+    			can be done with $content = $("<div>").append($input, $button); */
     		}
+    		$("main . content").append($content);
 
     		return false;
     	});
 
     });
+     $(".tabs a:first-child span").trigger("click");
 };
-
+$(document).ready(main);
