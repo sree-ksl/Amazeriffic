d4266c09e5032ea876dca4eb91b8becc10f8f97f
DS <grok.ksl@gmail.com>
2014-12-02 11:29:42 +0530

    use the nth child selector to determine the tab selected

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,9 +5,22 @@ var main = function() {
     $(".tabs a span").toArray().forEach(function(element){
     	//create a click handler for this element
     	$(element).on("click", function(){
+    		//since we are using the jQuery version of element, we'll create a temp variableso we don't need to keep recreating it
+    		var $element = $(element);
+
     		$(".tabs a span").removeClass("active");
     		$(element).addClass("active");
     		$("main .content").empty();
+
+    		if($element.parent().is(":nth-child(1)")){
+    			console.log("first tab clicked");
+    		}else if ($element.parent().is(":nth-child(2)")){
+    			console.log("second tab clicked");
+    		}
+    		else if ($element.parent().is(":nth-child(3)")){
+    			console.log("third tab clicked");
+    		}
+    		
     		return false;
     	});
 
